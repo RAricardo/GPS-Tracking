b69e5c1654ca17f74e4cc73080cc2d44218b42e1
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2019-02-12 01:08:32 -0500

    Más mejoras a experiencia de usuario.

--- public/js/dibujar.js
+++ public/js/dibujar.js
@@ -4,42 +4,55 @@ var link = $("#googleAPI").attr("src");
 $("#googleAPI").attr("src", link + config.GOOGLE_KEY);
 
 $("#botonVer").click(function () {
-  var nombre = $("#select").val();
-  var data={
-      option: nombre,
-}
+  var nombre = $("#selectRuta").val();
+  console.log(nombre);
+  var data = {
+    option: nombre,
+  }
 
-  var url="/track/obtenerRuta";
-  $.post(url, data, function(data, status){
-      data.puntos.forEach(punto => {
-        var lat = Number(punto.lat);
-        var lng = Number(punto.lon);
-        valores.push({lat, lng});
-        console.log(lat);
-        console.log(lng);
-      }); 
+  var url = "/track/obtenerRuta";
+  $.post(url, data, function (data, status) {
+    data.puntos.forEach(punto => {
+      var lat = Number(punto.lat);
+      var lng = Number(punto.lon);
+      valores.push({ lat, lng });
+      initMap(valores);
+    });
   });
 });
 
-$("#botonDibujar").click(function(e){
-    initMap(valores);
+$("#botonCompartir").click(function () {
+  var nombreRuta = $("#selectRuta").val();
+  var nombreUsuario = $("#selectuser").val();
+
+  var data = {
+    user: nombreUsuario,
+    ruta: nombreRuta,
+  }
+
+  var url = "/track/compartir";
+  
+  $.post(url, data, function (data, status) {
+    $(".alert").toggleClass('show');
+  });
+  
 });
 
 function initMap(valores) {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 20,
-      center: {lat: valores[0].lat, lng: valores[0].lng},
-      mapTypeId: 'terrain',
-      gestureHandling: 'cooperative'
-    });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 20,
+    center: { lat: valores[0].lat, lng: valores[0].lng },
+    mapTypeId: 'terrain',
+    gestureHandling: 'cooperative'
+  });
 
-    var flightPath = new google.maps.Polyline({
-      path: valores,
-      geodesic: true,
-      strokeColor: '#FF4000',
-      strokeOpacity: 1.0,
-      strokeWeight: 10
-    });
+  var flightPath = new google.maps.Polyline({
+    path: valores,
+    geodesic: true,
+    strokeColor: '#FF4000',
+    strokeOpacity: 1.0,
+    strokeWeight: 10
+  });
 
-    flightPath.setMap(map);
+  flightPath.setMap(map);
 }
--- routes/track.js
+++ routes/track.js
@@ -7,13 +7,15 @@ router.get("/", isLoggedIn, function (req, res) {
     res.render("track/index");
 });
 
-router.get("/ver",isLoggedIn, function (req, res) {
+router.get("/ver", isLoggedIn, function (req, res) {
     User.findById(req.user._id).populate("rutas").exec(function (err, user) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("track/visualizar", { user: user });
-        }
+        User.find({}, function (err, users) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("track/visualizar", { user: user, users: users});
+            }
+        })
     })
 });
 
@@ -29,13 +31,30 @@ function isLoggedIn(req, res, next) {
     }
 }
 
-router.post("/obtenerRuta", isLoggedIn, function(req, res){
-    Ruta.findOne( {nombre: req.body.option }, function(err, ruta){
-        if(err){
+router.post("/obtenerRuta", isLoggedIn, function (req, res) {
+    Ruta.findOne({ nombre: req.body.option }, function (err, ruta) {
+        if (err) {
             console.log(err);
         } else {
             res.send(ruta);
         }
+    })
+})
+
+router.post("/compartir", isLoggedIn, function (req, res) {
+    User.findOne({ username: req.body.user }).populate("rutas").exec(function (err, usuarioDestino) {
+        User.findById(req.user._id).populate("rutas").exec(function (err, currentUser) {
+            var currentUserRoutes = currentUser.rutas;
+            var trueRoute;
+            currentUserRoutes.forEach(ruta => {
+                if (ruta.nombre === req.body.ruta) {
+                    trueRoute = ruta;
+                }
+            })
+            usuarioDestino.rutas.push(trueRoute);
+            usuarioDestino.save();
+            res.sendStatus(200);
+        });
     })
 })
 
